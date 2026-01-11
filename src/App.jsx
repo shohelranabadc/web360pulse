a93b0c39d7d91a8d5f9862c969048c3e57545ab6
Mohammad Shohel Rana <47520471+shohelranabadc@users.noreply.github.com>
2026-01-11 12:32:03 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+
+
+function App() {
+  return (
+    <div>
+      <h1 className="my-5 mx-3 text-amber-500 text-center text-4xl">project set up file react with tailwind css</h1>
+      
+    </div>
+  )
+}
+
+export default App
